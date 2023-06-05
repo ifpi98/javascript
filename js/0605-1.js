b3a7f95243992c9a12c0d70b134097396263b903
TQ2L774\학생 <abc@a.com>
2023-06-05 18:00:21 +0900

    오후 수업 내용 업데이트

--- js/0605-1.js
+++ js/0605-1.js
@@ -27,19 +27,63 @@ $('h3').filter(function (index){
 
 function stock(money, month ,rate){
     for (var j = 0; j < month; j++){
+        premoney = money;
+        // console.log("이번 달 시작 금액 :"+premoney)
         for (var i = 0; i < 20 ; i++){
             money = money + (money * rate /100);
         }
         money = money-1000000;
+        
+        console.log("이번 달 잔고 :"+money)
+        console.log("번 돈 :"+(money-premoney));
     }
     return money;
 }
 
-console.log(stock(2000000,6,2));
-
-
-
+// console.log(stock(3000000,12,2));
 
 // filter, end, eq, first, last, add
 // is, find()
 // 문서객체선택과 탐색
+
+$(function(){
+
+    var xml = "";
+    xml += '<friends>';
+    xml += '<friend>';
+    xml += '<name>강감찬</name>';
+    xml += '<lang>HTML</lang>';
+    xml += '</friend>';
+    xml += '<friend>';
+    xml += '<name>이순신</name>';
+    xml += '<lang>CSS</lang>';
+    xml += '</friend>';
+    xml += '</friends>';
+    console.log(xml);
+
+    var str2xml = $.parseXML(xml);
+    // each문을 이용해 xml데이터를 화면에 출력하기
+    $(str2xml).find('friends').each(function(index){
+        var out='';
+        out += "<p>" + $(this).find('name').text() + "</p>"
+        out += "<strong>" + $(this).find('lang').text() + "</p>" + "</strong>"
+        console.log(out);
+        $('body').append(out);
+    })
+
+});
+
+var request = new XMLHttpRequest();
+var url = 'https://www.weather.go.kr/weather/forecast/mid-term-rss3.jsp?stnId=108'; // 받아올 XML 파일의 주소
+
+request.open('GET', url, true);
+request.onreadystatechange = function() {
+  if (request.readyState === 4 && request.status === 200) {
+    var xmlData = request.responseXML; // XML 데이터를 변수에 저장
+    // 원하는 작업을 수행하거나 변수를 활용할 수 있습니다.
+    console.log(xmlData);
+  }
+};
+request.send();
+
+
